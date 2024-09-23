6f4b929dd7b3b90dae258e8dc46e35ac6a9869ca
brunamilhomem99 <120478206+brunamilhomem99@users.noreply.github.com>
2024-09-23 17:18:37 -0300

    Ajustes

--- pokemon.cpp
+++ pokemon.cpp
@@ -4,10 +4,10 @@
 #include <ctime>
 
 // Construtor padrão
-Pokemon::Pokemon() : nome(""), tipo1(""), tipo2(""), hp(""), nivel(""), ataque(""), defesa(""), velocidade(""), ataque_especial(""), defesa_especial("") {}
+Pokemon::Pokemon() : nome(""), tipo1(""), tipo2(""), hp(0), nivel(0), ataque(0), defesa(0), velocidade(0), ataque_especial(0), defesa_especial(0) {}
 
 // Construtor com parâmetros
-Pokemon::Pokemon(const string& n, const string& tp1, const string& tp2, int hp, int nvl, int atq, int dfs, int v, int atq_espcial, int dfs_especial) : nome(n), tipo1(tp1), tipo2(tp2), hp(hp), nivel(nvl), ataque(atq), defesa(dfs), velocidade(v), ataque_especial(atq_especial), defesa_especial(dfs_especial) {}
+Pokemon::Pokemon(const string& n, const string& tp1, const string& tp2, int hp, int nvl, int atq, int dfs, int v, int atq_especial, int dfs_especial) : nome(n), tipo1(tp1), tipo2(tp2), hp(hp), nivel(nvl), ataque(atq), defesa(dfs), velocidade(v), ataque_especial(atq_especial), defesa_especial(dfs_especial) {}
 
 // Getters
 string Pokemon::getNome() const {return nome;}
@@ -22,28 +22,28 @@ int Pokemon::getAtaqueEspecial() const {return ataque_especial;}
 int Pokemon::getDefesaEspecial() const {return defesa_especial;}
 
 // Setters
-void Pokemon::setHP(int hp) {hp = hp;}
+void Pokemon::setHP(int hp) {this -> hp = hp;}
 void Pokemon::setNivel(int nvl) {nivel = nvl;}
 void Pokemon::setAtaque(int atq) {ataque = atq;}
 void Pokemon::setDefesa(int dfs) {defesa = dfs;}
-void setVelocidade(int v) {velocidade = v;}
-void setAtaqueEspecial(int atq_especial) {ataque_especial = atq_especial;}
-void setDefesaEspecial(int dfs_especial) {defesa_especial = dfs_especial;}
+void Pokemon::setVelocidade(int v) {velocidade = v;}
+void Pokemon::setAtaqueEspecial(int atq_especial) {ataque_especial = atq_especial;}
+void Pokemon::setDefesaEspecial(int dfs_especial) {defesa_especial = dfs_especial;}
 
 // Métodos para carregar e sortear os pokemons do arquivo txt
-vector<Pokemon> carregarPokemons(const string& nove_arquivo) {
+vector<Pokemon> carregarPokemons(const string& nome_arquivo) {
     vector<Pokemon> pokemons;
     ifstream arquivo(nome_arquivo);
 
-    if (!arquivo.is_open) {
+    if (!arquivo.is_open()) {
         cerr << "Erro ao abrir o arquivo!" << endl;
-        return 1;
+        return pokemons; // retorna o vetor vazio em caso de erro
     }
 
     string nome, tipo1, tipo2;
     int hp, nivel, ataque, defesa, velocidade, ataque_especial, defesa_especial;
 
-    while (nome >> tipo1 >> tipo2 >> hp >> nivel >> defesa >> velocidade >> ataque_espeical >> defesa_especial) {
+    while (arquivo >> nome >> tipo1 >> tipo2 >> hp >> nivel >> defesa >> velocidade >> ataque_especial >> defesa_especial) {
         pokemons.emplace_back(nome, tipo1, tipo2, hp, nivel, ataque, defesa, velocidade, ataque_especial, defesa_especial);
     }
 
@@ -53,7 +53,18 @@ vector<Pokemon> carregarPokemons(const string& nove_arquivo) {
 int main() {
     vector<Pokemon> vetor_pokemons = carregarPokemons("pokemons.txt");
 
-    cout << vetor_pokemons << endl;
+    for (const auto& pokemon : vetor_pokemons) {
+    cout << "Nome: " << pokemon.getNome()
+            << ", Tipo1: " << pokemon.getTipo1()
+            << ", Tipo2: " << pokemon.getTipo2()
+            << ", HP: " << pokemon.getHP()
+            << ", Nível: " << pokemon.getNivel()
+            << ", Ataque: " << pokemon.getAtaque()
+            << ", Defesa: " << pokemon.getDefesa()
+            << ", Velocidade: " << pokemon.getVelocidade()
+            << ", Ataque Especial: " << pokemon.getAtaqueEspecial()
+            << ", Defesa Especial: " << pokemon.getDefesaEspecial() << endl;
+}
 
     return 0;
 }
